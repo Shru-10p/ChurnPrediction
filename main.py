25bd6a3af09c74f76c2156c3cb6ac66e4abb697b
Shruten Pande <shrutenp@gmail.com>
2025-03-24 09:23:29 +0530

    Edit main file

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score
 import joblib
 from flask import Flask, request, render_template
 
@@ -30,35 +30,51 @@ def load_and_preprocess_data():
 def train_modelRandomForest(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    models = [RandomForestClassifier(n_estimators=100, random_state=42), LogisticRegression(), KNeighborsClassifier(), SVC()]
-    for model in models:
-        model.fit(X_train, y_train)
-        y_pred = model.predict(X_test)
-        accuracy = accuracy_score(y_test, y_pred)
-        # print('Model accuracy: {}'.format(accuracy))
-        # print('Classification Report:')
-        # print(classification_report(y_test, y_pred))
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
 
-    joblib.dump(model, 'random_forest_model.pkl')
+    return model
 
+def train_modelLogisticRegression(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+
+    return model
+
+def train_modelKNN(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    model = KNeighborsClassifier()
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+
+    return model
 
 app = Flask(__name__)
 
 X, y = load_and_preprocess_data()
 
-train_modelRandomForest(X, y)
+model_rf = train_modelRandomForest(X, y)
+model_lr = train_modelLogisticRegression(X, y)
+model_knn = train_modelKNN(X, y)
 
-model = joblib.load('random_forest_model.pkl')
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method=="GET":
         return render_template('index.html')
     features = [float(x) for x in request.form.values()]
-    prediction = model.predict([features])
-    print(prediction)
-    return render_template('index.html', prediction=prediction[0])
+    prediction_rf = model_rf.predict([features])
+    prediction_lr = model_lr.predict([features])
+    prediction_knn = model_knn.predict([features])
+    return render_template('index.html', prediction_rf=prediction_rf, prediction_lr=prediction_lr, prediction_knn=prediction_knn)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
